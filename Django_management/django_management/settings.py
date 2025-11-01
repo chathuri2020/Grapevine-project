9387b08fdfc8376d9a6af7b43ae6d10b57c0f292
chathuri2020 <71623402+chathuri2020@users.noreply.github.com>
2025-11-02 00:30:45 +0530

    t

--- Django_management/django_management/settings.py
+++ Django_management/django_management/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'PORT': os.getenv("DB_PORT", "33060"),
     }
 }
 
