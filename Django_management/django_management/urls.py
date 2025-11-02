8a3f33c627420a3f77bb9b344343f2c04176309a
chathuri2020 <71623402+chathuri2020@users.noreply.github.com>
2025-11-02 21:38:32 +0530

    url file change for debug

--- Django_management/django_management/urls.py
+++ Django_management/django_management/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
 ]
 
 # Adding media as a url
-#if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
