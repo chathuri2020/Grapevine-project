42d6eeebb4143e5643872d5dc9462362ea4b4229
chathuri2020 <71623402+chathuri2020@users.noreply.github.com>
2025-11-02 12:32:37 +0530

    db_chnged

--- Django_management/django_management/settings.py
+++ Django_management/django_management/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'django_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT", "33060"),
+        'NAME': 'vitisai',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'srv-captain--vitisai-db',
+        'PORT': '3306',
     }
 }
 
