e99044bcb30b3c49921ab50018a43961549b365c
chathuri2020 <71623402+chathuri2020@users.noreply.github.com>
2025-11-02 20:37:07 +0530

    media-file-fixing

--- Django_management/django_management/settings.py
+++ Django_management/django_management/settings.py
@@ -130,7 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media Files Settings
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
